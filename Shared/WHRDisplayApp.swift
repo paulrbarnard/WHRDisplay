1fc4616ec7de93d94cf6852d63dd8edc6b93ddc0
Paul Barnard <barnard@ygomi.com>
2021-08-20 14:38:17 +0100

    Initial Commit

---
+++ Shared/WHRDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WHRDisplayApp.swift
+//  Shared
+//
+//  Created by Paul Barnard on 20/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct WHRDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
